cad85d19dfb4cdd1371a695c7940fbd923687ebe
AbuShl123 <shoalievabubakr@gmail.com>
2025-07-12 11:07:09 +0500

    Small bugfix

--- src/utils/EnvParams.js
+++ src/utils/EnvParams.js
@@ -2,7 +2,7 @@
 const IP_ADDRESS = 'tc-screener.com';
 // const IP_ADDRESS = 'localhost:1105';
 
-const SSL = '';
+const SSL = 's';
 
 export const BASE_WS_URL='ws' + SSL + '://' + IP_ADDRESS + '/ws';
 export const BASE_URL='http' + SSL + '://' + IP_ADDRESS + '/api/v1';
